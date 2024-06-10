f673fae6001cfecb2df1c8caf2d25f17b3e9f9b7
Suebin Kim <k.sue3028@gmail.com>
2024-06-10 23:01:42 +0900

    feat: add animation

--- src/components/AlonePage.tsx
+++ src/components/AlonePage.tsx
@@ -9,10 +9,9 @@ import Text from "./Text";
 
 const AlonePage = () => {
   const divRef = useRef<HTMLDivElement>(null);
-  const data = useRef<{
-    data: { year: number; value: number }[];
-    svgs: Node[];
-  }>();
+  const divRef1 = useRef<HTMLDivElement>(null);
+  const divRef2 = useRef<HTMLDivElement>(null);
+  const data = useRef<{ year: number; value: number }[]>();
   const { goDown, lock } = useScrollContext();
 
   const [year, setYear] = useState(2000);
@@ -23,9 +22,9 @@ const AlonePage = () => {
 
   const loadData = async () => {
     if (!data.current) {
-      const d = await d3.dsv(",", "/data/live_alone.csv");
+      data.current = await d3.dsv(",", "/data/live_alone.csv");
 
-      const svgs = [
+      const [aloneSvg, ...otherSvgs] = [
         (await d3.svg("live_alone.svg")).documentElement as Node,
         (await d3.svg("live_other0.svg")).documentElement as Node,
         (await d3.svg("live_other1.svg")).documentElement as Node,
@@ -33,43 +32,54 @@ const AlonePage = () => {
         (await d3.svg("live_other3.svg")).documentElement as Node,
         (await d3.svg("live_other4.svg")).documentElement as Node,
       ];
-      data.current = { data: d, svgs };
+
+      Array.from({ length: 100 }, (_, i) => {
+        if (!divRef1.current || !divRef2.current) return;
+
+        // console.log(i);
+
+        d3.select(divRef1.current?.appendChild(aloneSvg.cloneNode(true)))
+          .style("width", 50)
+          .style("height", 50)
+          .attr("width", 50)
+          .attr("height", 50)
+          .style("opacity", 0)
+          .attr("dx", i * 300 + (i - 1) * 20)
+          .style("transition", "1s linear");
+
+        d3.select(
+          divRef2.current?.appendChild(otherSvgs[i % 4].cloneNode(true))
+        )
+          .style("width", 50)
+          .style("opacity", 0)
+          .style("height", 50)
+          .attr("width", 50)
+          .attr("height", 50)
+          .attr("dx", i * 300 + (i - 1) * 20)
+          .style("transition", "1s linear");
+      });
+
       render(2000);
     }
   };
+
   const render = async (currentYear: number) => {
     if (!data.current) return;
-    const item = data.current.data.find(
-      ({ year: _year }) => +_year === currentYear
-    );
+    console.log("------");
+    const item = data.current.find(({ year: _year }) => +_year === currentYear);
     if (!item) return;
 
     const alone = Math.round(item.value);
 
-    const [aloneSvg, ...otherSvgs] = data.current.svgs;
-
-    if (divRef.current) divRef.current.innerHTML = "";
+    const aloneSvgs = divRef1.current?.querySelectorAll("svg");
+    const otherSvgs = divRef2.current?.querySelectorAll("svg");
+    console.log("jwl3ofeijaw;oifjew;al");
+    console.log({ aloneSvgs });
+    if (!aloneSvgs || !otherSvgs) return;
 
     Array.from({ length: 100 }, (_, i) => {
-      if (!divRef.current) return;
-
-      // console.log(i);
-      if (i <= alone) {
-        d3.select(divRef.current?.appendChild(aloneSvg.cloneNode(true)))
-          .style("width", 50)
-          .style("height", 50)
-          .attr("width", 50)
-          .attr("height", 50)
-          .attr("dx", i * 300 + (i - 1) * 20);
-      } else {
-        d3.select(divRef.current?.appendChild(otherSvgs[i % 4].cloneNode(true)))
-          .style("width", 50)
-          .style("opacity", 0.15)
-          .style("height", 50)
-          .attr("width", 50)
-          .attr("height", 50)
-          .attr("dx", i * 300 + (i - 1) * 20);
-      }
+      aloneSvgs.item(i).style.opacity = i < alone ? "1" : "0";
+      otherSvgs.item(i).style.opacity = i < alone ? "0" : "0.3";
     });
   };
 
@@ -92,7 +102,7 @@ const AlonePage = () => {
         setYear((y) => y + 1);
         yearRef.current += 1;
         render(yearRef.current);
-      }, 500);
+      }, 200);
     }
   }, [showChange]);
 
@@ -112,15 +122,40 @@ const AlonePage = () => {
         justifyContent={"flex-start"}
         alignItems={"flex-start"}
       >
-        <Text type={"Medium"} fontSize={24} p={20}>
-          {year}년의 1인 가구 비율
+        <Text type={"Medium"} fontSize={40} color={COLORS.BLUE}>
+          {year}년
+        </Text>
+        <Text type={"Medium"} fontSize={34}>
+          1인 가구 비율
         </Text>
         <Flex
-          ref={divRef}
-          flexWrap={"wrap"}
-          alignItems={"flex-start"}
-          justifyContent={"flex-start"}
-        ></Flex>
+          flexGrow={1}
+          w="100%"
+          mb={50}
+          position={"relative"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Flex
+            pos={"absolute"}
+            ref={divRef1}
+            flexWrap={"wrap"}
+            alignItems={"flex-start"}
+            justifyContent={"center"}
+            width="60%"
+            height="60%"
+          ></Flex>
+          <Flex
+            pos={"absolute"}
+            flex={1}
+            ref={divRef2}
+            flexWrap={"wrap"}
+            alignItems={"flex-start"}
+            justifyContent={"center"}
+            width="60%"
+            height="60%"
+          ></Flex>
+        </Flex>
       </Flex>
     </PageTemplate>
   );
